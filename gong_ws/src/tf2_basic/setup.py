f9abe4624fd3c514362feacb5d42214e8e948694
jin0608-s <luckymijin0608@gmail.com>
2026-07-27 16:36:20 +0900

    git ignore_git

--- gong_ws/src/tf2_basic/setup.py
+++ gong_ws/src/tf2_basic/setup.py
@@ -35,6 +35,7 @@ setup(
             "turtle_tf_listener = tf2_basic.turtle_tf_listener:main",
             "move_u2d2 = tf2_basic.move_u2d2:main",
             "move_manipulator = tf2_basic.move_manipulator:main",
+            "dance_manipulator = tf2_basic.dance_manipulator:main",
             
         ],
     },
