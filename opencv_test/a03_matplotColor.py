3f333caa7d651a08157231765bf1da9225a7de01
jin0608-s <luckymijin0608@gmail.com>
2026-08-03 16:22:51 +0900

    camera

--- opencv_test/a03_matplotColor.py
+++ opencv_test/a03_matplotColor.py
@@ -1,21 +1,21 @@
+# pip install matplotlib
+
 from pathlib import Path
 
 import cv2
-import numpy as np
 from matplotlib import pyplot as plt
+
 
 def main():
     file_path = Path(__file__).parent
     img = cv2.imread(str(file_path / "data/robot.jpeg"))
-    cv2.imshow("robot", img)        #검정 화면 뛰우기
-    plt.axis("off")  # 좌표축 제거
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # BGR -> RGB
-    img.resize((400, 400, 3))
+    cv2.imshow("robot", img)
+    plt.axis("off")
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     plt.imshow(imgRGB)
+    cv2.waitKey(30)
     plt.show()
-    cv2.waitKey()   # 블럭 함수
-    
-    
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
--- opencv_test/data/a00_default.py
+++ opencv_test/data/a00_default.py
@@ -3,12 +3,13 @@ from pathlib import Path
 import cv2
 import numpy as np
 
+
 def main():
     file_path = Path(__file__).parent
-    black_img = np.array((300, 300, 1), dtype=np.uint8)  # 검정 화면
-    cv2.imshow("black", black_img)        #검정 화면 뛰우기
-    cv2.waitKey()   # 블럭 함수
-    
+    black_img = np.zeros((300, 300, 1), dtype=np.uint8)
+    cv2.imshow("balck", black_img)
+    cv2.waitKey()  # 블럭 함수
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
